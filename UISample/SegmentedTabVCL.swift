456c1cc2df45da36c9c62ec4307580a034026fd7
hiratsukashu <hiratsukashu@indival.co.jp>
2016-03-23 20:30:20 +0900

    一旦途中まで

---
+++ UISample/SegmentedTabVCL.swift
@@ -0,0 +1,38 @@
+//
+//  SegmentedTabVCL.swift
+//  UISample
+//
+//  Created by 平塚 俊輔 on 2016/03/23.
+//  Copyright © 2016年 平塚 俊輔. All rights reserved.
+//
+
+import UIKit
+
+class SegmentedTabVCL: UIViewController {
+
+    @IBOutlet weak var pageCtrl: UISegmentedControl!
+    @IBOutlet weak var increaseNavBar: UIToolbar!
+    @IBOutlet weak var pageView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
